e7243083f619db48384923686406fec5d539e488
xujiashuai <xujiashuai@keruyun.com>
2020-12-04 11:09:27 +0800

    demo1

---
+++ src/main/java/com/github/xjs/douyin/demo1/IntegerTest.java
@@ -0,0 +1,12 @@
+package com.github.xjs.douyin.demo1;
+
+public class IntegerTest {
+    public static void main(String[] args) {
+        Integer i1 = 100;
+        Integer i2 = 100;
+        System.out.println(i1 == i2);
+        Integer i3 = 1000;
+        Integer i4 = 1000;
+        System.out.println(i3 == i4);
+    }
+}
